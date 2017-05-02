e65b7893ee666279ff04578b4800cef9ce82cd34
JesseHuBinBin <354550712@qq.com>
2017-05-02 10:51:44 +0800

    新建扉页

--- app/src/main/java/com/eternal_life/vrlife/vr_life/activity/MainActivity.java
+++ app/src/main/java/com/eternal_life/vrlife/vr_life/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.eternal_life.vrlife.vr_life;
+package com.eternal_life.vrlife.vr_life.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.eternal_life.vrlife.vr_life.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///Hello World!
+
 
     }
 }
---
+++ app/src/main/java/com/eternal_life/vrlife/vr_life/activity/TitlePaperActivity.java
@@ -0,0 +1,18 @@
+package com.eternal_life.vrlife.vr_life.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.eternal_life.vrlife.vr_life.R;
+
+/**
+ * Created by Huwenbin on 2017/5/2.
+ */
+
+public class TitlePaperActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.title_paper_activity);
+    }
+}
